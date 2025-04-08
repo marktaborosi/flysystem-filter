e54ea4d810ecf8baca238629698394076b4989c8
Mark Taborosi <mark.taborosi@gmail.com>
2025-04-08 21:20:25 +0200

    🧪 Add more unit test cases for FilterBuilder and FlysystemFilter

--- tests/Unit/FilterBuilderTest.php
+++ tests/Unit/FilterBuilderTest.php
@@ -2,6 +2,7 @@
 namespace Marktaborosi\FlysystemFilter\Tests\Unit;
 
 use Carbon\Carbon;
+use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
 use League\Flysystem\StorageAttributes;
 use League\Flysystem\FileAttributes;
@@ -18,7 +19,7 @@ class FilterBuilderTest extends TestCase
     /**
      * Test filtering only files.
      *
-     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws Exception
      */
     public function test_is_file()
     {
@@ -35,7 +36,7 @@ class FilterBuilderTest extends TestCase
     /**
      * Test filtering only directories.
      *
-     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws Exception
      */
     public function test_is_directory()
     {
@@ -52,7 +53,7 @@ class FilterBuilderTest extends TestCase
     /**
      * Test filtering by exact path match.
      *
-     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws Exception
      */
     public function test_path_equals()
     {
@@ -68,7 +69,7 @@ class FilterBuilderTest extends TestCase
     /**
      * Test filtering by path containing a substring.
      *
-     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws Exception
      */
     public function test_path_contains()
     {
@@ -84,7 +85,7 @@ class FilterBuilderTest extends TestCase
     /**
      * Test filtering by exact filename match (without extension).
      *
-     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws Exception
      */
     public function test_filename_equals()
     {
@@ -100,7 +101,7 @@ class FilterBuilderTest extends TestCase
     /**
      * Test filtering by exact extension match.
      *
-     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws Exception
      */
     public function test_extension_equals()
     {
@@ -116,7 +117,7 @@ class FilterBuilderTest extends TestCase
     /**
      * Test filtering by file size between two values.
      *
-     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws Exception
      */
     public function test_size_between()
     {
@@ -132,7 +133,7 @@ class FilterBuilderTest extends TestCase
     /**
      * Test filtering by last modified date between two timestamps.
      *
-     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws Exception
      */
     public function test_last_modified_between()
     {
@@ -148,7 +149,7 @@ class FilterBuilderTest extends TestCase
     /**
      * Test grouping of conditions.
      *
-     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws Exception
      */
     public function test_group_conditions()
     {
@@ -171,7 +172,7 @@ class FilterBuilderTest extends TestCase
     /**
      * Test combining multiple conditions.
      *
-     * @throws \PHPUnit\Framework\MockObject\Exception
+     * @throws Exception
      */
     public function test_combined_conditions()
     {
@@ -184,4 +185,160 @@ class FilterBuilderTest extends TestCase
 
         $this->assertTrue($filter->matches($item));
     }
+
+    /**
+     * OR operator test
+     *
+     * @throws Exception
+     */
+    public function test_or_operator_works()
+    {
+        $filter = new FilterBuilder();
+        $filter->filenameEquals('readme')->or()->extensionEquals('md');
+
+        $item = $this->createMock(StorageAttributes::class);
+        $item->method('path')->willReturn('/doc/readme.md');
+
+        $this->assertTrue($filter->matches($item));
+    }
+
+    /**
+     * Path not equals
+     * @throws Exception
+     */
+    public function test_path_not_equals()
+    {
+        $filter = new FilterBuilder();
+        $filter->pathNotEquals(['/exclude/this.txt']);
+
+        $item = $this->createMock(StorageAttributes::class);
+        $item->method('path')->willReturn('/include/that.txt');
+
+        $this->assertTrue($filter->matches($item));
+    }
+
+    /**
+     * Basename not contains
+     * @throws Exception
+     */
+    public function test_basename_not_contains()
+    {
+        $filter = new FilterBuilder();
+        $filter->basenameNotContains(['temp']);
+
+        $item = $this->createMock(StorageAttributes::class);
+        $item->method('path')->willReturn('/files/final_report.txt');
+
+        $this->assertTrue($filter->matches($item));
+    }
+
+    /**
+     * Group end with start throws
+     */
+    public function test_group_end_without_start_throws()
+    {
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('Mismatched group_end');
+
+        $filter = new FilterBuilder();
+        $filter->group_end();
+    }
+
+    /**
+     * Unclosed group throws
+     * @throws Exception
+     */
+    public function test_unclosed_group_throws()
+    {
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('Unbalanced groups');
+
+        $filter = new FilterBuilder();
+        $filter->group_start()->extensionEquals('txt');
+
+        $item = $this->createMock(StorageAttributes::class);
+        $item->method('path')->willReturn('/some/file.txt');
+
+        $filter->matches($item); // group_end hiányzik
+    }
+
+    /**
+     * Size equals with string format
+     * @throws Exception
+     */
+    public function test_size_equals_with_string_format()
+    {
+        $filter = new FilterBuilder();
+        $filter->sizeEquals('1M'); // 1 MB
+
+        $item = $this->createMock(FileAttributes::class);
+        $item->method('fileSize')->willReturn(1024 * 1024);
+
+        $this->assertTrue($filter->matches($item));
+    }
+
+    /**
+     * Metadata equals
+     * @throws Exception
+     */
+    public function test_metadata_equals()
+    {
+        $filter = new FilterBuilder();
+        $filter->metadataEquals('type', 'invoice');
+
+        $item = $this->createMock(StorageAttributes::class);
+        $item->method('extraMetadata')->willReturn(['type' => 'invoice']);
+
+        $this->assertTrue($filter->matches($item));
+    }
+
+    /**
+     * Visibility filters
+     * @throws Exception
+     */
+    public function test_visibility_filters()
+    {
+        $publicFilter = (new FilterBuilder())->isPublic();
+        $privateFilter = (new FilterBuilder())->isPrivate();
+
+        $publicItem = $this->createMock(StorageAttributes::class);
+        $publicItem->method('visibility')->willReturn('public');
+
+        $privateItem = $this->createMock(StorageAttributes::class);
+        $privateItem->method('visibility')->willReturn('private');
+
+        $this->assertTrue($publicFilter->matches($publicItem));
+        $this->assertTrue($privateFilter->matches($privateItem));
+    }
+
+    /**
+     * Extension contains
+     * @throws Exception
+     */
+    public function test_extension_contains()
+    {
+        $filter = new FilterBuilder();
+        $filter->extensionContains('x');
+
+        $item = $this->createMock(StorageAttributes::class);
+        $item->method('path')->willReturn('/some/file.txt');
+
+        $this->assertTrue($filter->matches($item));
+    }
+
+    /**
+     * Filename matches regex
+     * @throws Exception
+     */
+    public function test_filename_matches_regex()
+    {
+        $filter = new FilterBuilder();
+        $filter->filenameMatchesRegex('/^report_\d+$/');
+
+        $item = $this->createMock(StorageAttributes::class);
+        $item->method('path')->willReturn('/archive/report_123.txt');
+
+        $this->assertTrue($filter->matches($item));
+    }
+
 }
--- tests/Unit/FlysystemFilterTest.php
+++ tests/Unit/FlysystemFilterTest.php
@@ -8,6 +8,7 @@ use League\Flysystem\FileAttributes;
 use League\Flysystem\DirectoryAttributes;
 use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 class FlysystemFilterTest extends TestCase
 {
@@ -92,4 +93,140 @@ class FlysystemFilterTest extends TestCase
         // Assert the filtered result is empty
         $this->assertCount(0, $filteredListing->toArray());
     }
+
+    /**
+     * Test filtering with only directories
+     * @throws Exception
+     */
+    public function test_filter_only_directories()
+    {
+        $file = $this->createMock(FileAttributes::class);
+        $file->method('path')->willReturn('test/file.txt');
+
+        $dir1 = $this->createMock(DirectoryAttributes::class);
+        $dir1->method('path')->willReturn('test/dir1');
+
+        $dir2 = $this->createMock(DirectoryAttributes::class);
+        $dir2->method('path')->willReturn('test/dir2');
+
+        $listing = new DirectoryListing([$file, $dir1, $dir2]);
+
+        $builder = new FilterBuilder();
+        $builder->isDirectory();
+
+        $filtered = FlysystemFilter::filter($listing, $builder);
+
+        $items = $filtered->toArray();
+        $this->assertCount(2, $items);
+        $this->assertContains($dir1, $items);
+        $this->assertContains($dir2, $items);
+    }
+
+    /**
+     * Test files with log extensions
+     * @throws Exception
+     */
+    public function test_filter_files_with_log_extension()
+    {
+        $file1 = $this->createMock(FileAttributes::class);
+        $file1->method('path')->willReturn('log/file1.log');
+
+        $file2 = $this->createMock(FileAttributes::class);
+        $file2->method('path')->willReturn('log/file2.txt');
+
+        $file3 = $this->createMock(FileAttributes::class);
+        $file3->method('path')->willReturn('log/file3.log');
+
+        $listing = new DirectoryListing([$file1, $file2, $file3]);
+
+        $builder = new FilterBuilder();
+        $builder->isFile()->and()->extensionEquals('log');
+
+        $filtered = FlysystemFilter::filter($listing, $builder);
+
+        $items = $filtered->toArray();
+        $this->assertCount(2, $items);
+        $this->assertContains($file1, $items);
+        $this->assertContains($file3, $items);
+    }
+
+    /**
+     * Dirs or txt files
+     * @throws Exception
+     */
+    public function test_filter_dirs_or_txt_files()
+    {
+        $file1 = $this->createMock(FileAttributes::class);
+        $file1->method('path')->willReturn('some/file1.txt');
+
+        $file2 = $this->createMock(FileAttributes::class);
+        $file2->method('path')->willReturn('some/file2.log');
+
+        $dir = $this->createMock(DirectoryAttributes::class);
+        $dir->method('path')->willReturn('some/dir');
+
+        $listing = new DirectoryListing([$file1, $file2, $dir]);
+
+        $builder = new FilterBuilder();
+        $builder->isDirectory()->or()->extensionEquals('txt');
+
+        $filtered = FlysystemFilter::filter($listing, $builder);
+
+        $items = $filtered->toArray();
+        $this->assertCount(2, $items);
+        $this->assertContains($file1, $items);
+        $this->assertContains($dir, $items);
+    }
+
+    /**
+     * Filter with non storage attribute objects
+     * @throws Exception
+     */
+    public function test_filter_with_non_storage_attribute_objects()
+    {
+        $invalidObject = new stdClass();
+
+        $file = $this->createMock(FileAttributes::class);
+        $file->method('path')->willReturn('test/file.txt');
+
+        $listing = new DirectoryListing([$file, $invalidObject]);
+
+        $builder = new FilterBuilder();
+        $builder->isFile();
+
+        $filtered = FlysystemFilter::filter($listing, $builder);
+
+        $items = $filtered->toArray();
+        $this->assertCount(1, $items);
+        $this->assertSame($file, $items[0]);
+    }
+
+    /**
+     * Case insensitive extension
+     * @throws Exception
+     */
+    public function test_filter_with_case_insensitive_extension()
+    {
+        $file1 = $this->createMock(FileAttributes::class);
+        $file1->method('path')->willReturn('doc/file1.TXT');
+
+        $file2 = $this->createMock(FileAttributes::class);
+        $file2->method('path')->willReturn('doc/file2.txt');
+
+        $file3 = $this->createMock(FileAttributes::class);
+        $file3->method('path')->willReturn('doc/file3.log');
+
+        $listing = new DirectoryListing([$file1, $file2, $file3]);
+
+        $builder = new FilterBuilder();
+        $builder->isFile()->and()->extensionEquals('txt');
+
+        $filtered = FlysystemFilter::filter($listing, $builder);
+
+        $items = $filtered->toArray();
+        $this->assertCount(2, $items);
+        $this->assertContains($file1, $items);
+        $this->assertContains($file2, $items);
+    }
+
 }
